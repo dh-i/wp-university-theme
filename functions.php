d0d29a73dc9eb8a72553f5171018821f7893ad4d
dh-i <dhanush.code1@gmail.com>
2022-07-21 17:00:01 +0530

    wp-university theme init(header,footer,single,page,index)

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+function university_files()
+{
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+    wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+}
+
+add_action('wp_enqueue_scripts', 'university_files');
---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+    <!-- <title>Fictional University</title> -->
+</head>
+
+<body>
+
+    <header class="site-header">
+        <div class="container">
+            <h1 class="school-logo-text float-left">
+                <a href="#"><strong>Fictional</strong> University</a>
+            </h1>
+            <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+            <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+            <div class="site-header__menu group">
+                <nav class="main-navigation">
+                    <ul>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Programs</a></li>
+                        <li><a href="#">Events</a></li>
+                        <li><a href="#">Campuses</a></li>
+                        <li><a href="#">Blog</a></li>
+                    </ul>
+                </nav>
+                <div class="site-header__util">
+                    <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
+                    <a href="#" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
+                    <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+                </div>
+            </div>
+        </div>
+    </header>
