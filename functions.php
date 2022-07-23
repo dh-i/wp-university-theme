59f7014c0adda50ab14ab7dbdddfb96cc7f5c139
dh-i <dhanush.code1@gmail.com>
2022-07-23 16:31:13 +0530

    add index,front-page,archive pages & custom query

--- functions.php
+++ functions.php
@@ -1,5 +1,4 @@
 <?php
-
 function university_files()
 {
     wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
@@ -10,6 +9,7 @@ function university_files()
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
+
 
 function university_features()
 {
--- header.php
+++ header.php
@@ -33,7 +33,7 @@
                         <li><a href="#">Programs</a></li>
                         <li><a href="#">Events</a></li>
                         <li><a href="#">Campuses</a></li>
-                        <li><a href="#">Blog</a></li>
+                        <li <?php if (get_post_type() == 'post') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/blog'); ?>">Blog</a></li>
                     </ul>
                 </nav>
                 <div class="site-header__util">
