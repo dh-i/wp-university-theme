b34d1de2db8eedc94d17c92e78aaaab65696795c
dh-i <dhanush.code1@gmail.com>
2022-09-02 20:27:51 +0530

    Setup Fallback Traditional Search

--- functions.php
+++ functions.php
@@ -59,6 +59,12 @@ function university_files()
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+
+
+    //this will give access to the universityData variable in our front end
+    wp_localize_script('main-university-js', 'universityData', array(
+        'root_url' => get_site_url(),
+    ));
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
